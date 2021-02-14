7fd14c1287cc3ee73ee649735301e8d2e1af0156
Bhuvan <gbhuvanhyd@gmail.com>
2021-02-14 18:27:49 +0530

    done

--- Personal Expenses/personal_expenses/lib/main.dart
+++ Personal Expenses/personal_expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,28 +14,49 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: ' New Video Game',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'PS5',
+      amount: 499.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment:MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text(
-                'CHART!',
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text(
+                  'CHART!',
+                ),
+                elevation: 5,
+                color: Colors.blue,
               ),
-              elevation: 5,
-              color: Colors.blue,
             ),
-          ),
-          Card(child: Text('List Of TX:'))
-        ],
-      ),
-    );
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
