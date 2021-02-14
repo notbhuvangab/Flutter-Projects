e16d11a4d4cde81cb3425c22df1c90017e26abe0
Bhuvan <gbhuvanhyd@gmail.com>
2021-02-14 19:11:01 +0530

    ok

--- Personal Expenses/personal_expenses/lib/main.dart
+++ Personal Expenses/personal_expenses/lib/main.dart
@@ -52,8 +52,19 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: transactions.map((tx) {
                 return Card(
-                  child: Text(tx.title),
-                );
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(children: <Widget>[
+                      Text(tx.title.toString()),
+                      Text(tx.date.toString()),
+                    ],)
+                  ],
+                ));
               }).toList(),
             ),
           ],
